edcbc42e773d91183c95f6706d20c60465f913aa
gh-colebritton <colebritton1022@gmail.com>
2026-02-25 21:41:26 -0500

    upload syncusb.bat

---
+++ syncusb.bat
@@ -0,0 +1,18 @@
+@echo off
+setlocal
+set "SOURCE=C:\Users\white\OneDrive\Documents\Sync_Main"
+set "LABEL=CDB-USB"
+
+:: Find USB drive letter by label using PowerShell
+for /f "tokens=*" %%i in ('powershell -NoProfile -Command "(Get-Volume | Where-Object { $_.FileSystemLabel -eq '%LABEL%' } | Select-Object -ExpandProperty DriveLetter)"') do set "DRIVE=%%i"
+
+if not defined DRIVE (
+    echo USB '%LABEL%' not found. Exiting...
+    exit /b
+)
+
+set "DEST=%DRIVE%:\Sync_USB"
+
+robocopy "%SOURCE%" "%DEST%" /MIR /R:2 /W:5 /FFT >nul
+
+exit /b
